c3514763a99b0f1c02ec331cf2e872abf375f46d
dude56987 <dude56987@users.noreply.github.com>
2014-11-04 13:11:26 -0500

    Improved the update-reboot script

--- update-reboot.sh
+++ update-reboot.sh
@@ -1,24 +1,25 @@
 # /bin/bash
 # check if any users are logged in
-for dir in /home/*; do
-	user=$(echo $dir | sed "s/\/home\///g")
-	echo "Logged in users:"
-	if users | grep $user; then
-		# if someone is logged in exit this script
-		echo 	
-		echo "Users are logged in, system will not reboot."
-		echo "To update and reboot use the command"
-		echo
-		echo "update --reboot"
-		echo
-		echo "This script is used for automated updates to"
-		echo "workstations."
-		exit 1
+while true;do
+	loggedIn=false;
+	for dir in /home/*; do
+		user=$(echo $dir | sed "s/\/home\///g")
+		echo "Logged in users:"
+		if users | grep $user; then
+			# if someone is logged in exit this script
+			echo "$user";
+			loggedIn=true;
+		fi
+	done
+	if [ $loggedIn == false ];then
+		# break the loop and run update if no users are logged in
+		break;
+	else
+		echo "Users are logged in, system will not reboot yet."
 	fi
+	sleep 90;
 done
-echo "############################################" >> /var/log/autoUpdateLog
-echo "# REBOOT AND UPDATE ON:" >> /var/log/autoUpdateLog
-date >> /var/log/autoUpdateLog
-update >> /var/log/autoUpdateLog
+# update the system, use autoclean logs to clear logs
+update --auto-clean-log
 # reboot the computer
 reboot
